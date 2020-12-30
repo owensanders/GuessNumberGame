f18d2871acdd2b254641298b147b9d250e4c356b
owensanders <64283938+owensanders@users.noreply.github.com>
2020-12-30 15:39:27 +0000

    Refactor

--- guessing_number_game/script.js
+++ guessing_number_game/script.js
@@ -18,23 +18,15 @@ document.querySelector('.check').addEventListener('click', function() {
             highScore = score;
             document.querySelector('.highscore').textContent = highScore;
         }
-    } else if (guess > secretNumber) {
+    } else if (guess !== secretNumber) {
         if(score > 1) {
-            document.querySelector('.message').textContent = 'Too High!';
+            document.querySelector('.message').textContent = guess > secretNumber ? 'Too High!' : 'Too Low!';
             score--;
             document.querySelector('.score').textContent = score;
         } else {
             document.querySelector('.message').textContent = 'You Lose the Game';
         }
-    } else if (guess < secretNumber) {
-        if(score > 1) {
-            document.querySelector('.message').textContent = 'Too Low!';
-            score--;
-            document.querySelector('.score').textContent = score;
-        } else {
-            document.querySelector('.message').textContent = 'You Lose the Game';
-        }
-    }
+    } 
 });
 
 document.querySelector('.again').addEventListener('click', function() {
